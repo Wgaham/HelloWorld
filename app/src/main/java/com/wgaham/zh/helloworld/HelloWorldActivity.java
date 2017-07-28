78bc22f04eba111098c5790aedde52407f0bc11d
Wgaham <jszzhaihao@live.com>
2017-07-28 19:45:46 +0800

    All

---
+++ app/src/main/java/com/wgaham/zh/helloworld/HelloWorldActivity.java
@@ -0,0 +1,18 @@
+package com.wgaham.zh.helloworld;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+public class HelloWorldActivity extends AppCompatActivity {
+    private static final String TAG = "HelloWorldActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hello_world_layout);
+        Log.d("data","onCreate execute");
+        //sqsqsqss
+        //Log.d(TAG, "onCreate: ");
+    }
+}
